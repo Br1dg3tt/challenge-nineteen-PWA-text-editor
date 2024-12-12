243d3adc927fd7d7429705424f0596c67df9dd2c
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-12-12 16:40:45 -0700

    added logic to server.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,13 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = await request;
+  console.log('Content added to the database');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
